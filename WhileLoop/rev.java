d21bda13780c2cf3b110cce4aba9c595a8529ef5
Akilan <pakilanpakilan281@gmail.com>
2025-07-09 20:25:26 +0530

    Add files via upload

---
+++ WhileLoop/rev.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class rev{
+    public static void main(String []args){
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        int temp=0;
+        while(b>0){
+            temp=b%10;
+                System.out.print(temp); 
+            b/=10;
+    }
+    }   
+ 
+}
